5dcee78bd941e07ddf7cfec69ddcf2143485d089
Arielson Oliveira <aryelnti@gmail.com>
2023-06-06 23:58:12 +0100

    implement backend

---
+++ escola-backend/webapi/app.js
@@ -0,0 +1,168 @@
+//app.js
+const {MongoClient, ObjectId} = require("mongodb");
+async function connect(){
+  if(global.db) return global.db;
+    const conn = await MongoClient.connect("mongodb+srv://dev-web01:NsXYphuwOPYnoXcU@dev-web01.k3qvh9b.mongodb.net/");
+  if(!conn) return new Error("Can't connect");
+    global.db = await conn.db("unifor");
+  return global.db;
+}
+
+const express = require('express');
+const app = express();         
+const port = 3000; //porta padrão
+
+app.use(require('cors')());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//definindo as rotas
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+// Generate
+const jwt = require('jsonwebtoken');
+
+function generateToken(userId) {
+  const token = jwt.sign({ userId }, 'seuSegredoAqui', { expiresIn: '1h' });
+  return token;
+}
+
+function authenticate(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ erro: 'Token de autenticação não fornecido' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'seuSegredoAqui');
+    req.userId = decoded.userId;
+    next();
+  } catch (ex) {
+    console.log(ex);
+    res.status(401).json({ erro: 'Token de autenticação inválido' });
+  }
+}
+
+// // Aplicar o middleware em todas as rotas que exigem autenticação
+// router.use(authenticate);
+
+
+// GET dog
+router.get('/dog', async function(req, res, next) {
+  try{
+    const apidog = await fetch('https://dog.ceo/api/breed/hound/list');
+    res.json(await apidog.json());
+  }
+  catch(ex){
+    console.log(ex);
+    res.status(400).json({erro: `${ex}`});
+  }
+}) 
+
+/* GET aluno */
+router.get('/aluno/:id?', authenticate, async function(req, res, next) {
+    try{
+      const db = await connect();
+      if(req.params.id)
+        res.json(await db.collection("aluno").findOne({_id: new ObjectId(req.params.id)}));
+      else
+        res.json(await db.collection("aluno").find().toArray());
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// POST /aluno
+router.post('/aluno', authenticate, async function(req, res, next){
+    try{
+      const aluno = req.body;
+      const db = await connect();
+      res.json(await db.collection("aluno").insertOne(aluno));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// POST /login
+router.post('/login', async function(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    
+    // Verificar as credenciais do usuário no banco de dados
+    const db = await connect();
+    const user = await db.collection("users").findOne({ email, password });
+    
+    if (!user) {
+      return res.status(401).json({ erro: 'Credenciais inválidas' });
+    }
+    
+    // Gerar e retornar um token de autenticação para o cliente
+    const token = generateToken(user._id);
+    res.json({ token });
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+// POST /signup
+router.post('/signup', async function(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    // Verificar se o usuário já está cadastrado no banco de dados
+    const db = await connect();
+    const existingUser = await db.collection("users").findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ erro: 'Usuário já cadastrado' });
+    }
+
+    // Cadastrar o novo usuário no banco de dados
+    const newUser = { email, password };
+    const result = await db.collection("users").insertOne(newUser);
+
+    res.json({ message: 'Usuário cadastrado com sucesso' });
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+// PUT /aluno/{id}
+router.put('/aluno/:id', async function(req, res, next){
+    try{
+      const aluno = req.body;
+      const db = await connect();
+      res.json(await db.collection("aluno").updateOne({_id: new ObjectId(req.params.id)}, {$set: aluno}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// DELETE /aluno/{id}
+router.delete('/aluno/:id', async function(req, res, next){
+    try{
+      const db = await connect();
+      res.json(await db.collection("aluno").deleteOne({_id: new ObjectId(req.params.id)}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
