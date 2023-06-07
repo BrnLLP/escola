bc8437751051dc58b918f70cb79d7ce8272c90d9
BrnLLP <bllp850@gmail.com>
2023-06-07 19:29:46 -0300

    Adição de formulario de cadastro na gestão administrativa

--- sistemaescolar/frontend/src/components/Users/Tabela.jsx
+++ sistemaescolar/frontend/src/components/Users/Tabela.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import axios, {isCancel, AxiosError} from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+    MDBContainer,
+  MDBTabs,
+    MDBTabsItem,
+    MDBTabsLink,
+    MDBTabsContent,
+    MDBTabsPane,
+    MDBBtn,
+    MDBIcon,
+    MDBInput,
+    MDBCheckbox
+  } from 'mdb-react-ui-kit';
 
 export default props =>
 <div>
@@ -18,4 +30,22 @@ export default props =>
 
 </tbody>
 </table>
+<script src='Cadastro.js'/>
+    <div id='output'>
+    <form id='form'>
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+
+      <MDBInput wrapperClass='mb-4' label='Usuário' id='usuario' type='text'/>
+      <MDBInput wrapperClass='mb-4' label='Senha' id='senha' type='password'/>
+      <MDBInput wrapperClass='mb-4' label='Matricula' id='matricula' type='number'/>
+      <MDBInput wrapperClass='mb-4' label='CPF' id='cpf' type='text'/>
+
+
+      <MDBBtn className="mb-4" id="btn" onclick='Cadastro()'>Cadastrar</MDBBtn>
+
+  
+
+    </MDBContainer>
+    </form>
+    </div>
 </div>
