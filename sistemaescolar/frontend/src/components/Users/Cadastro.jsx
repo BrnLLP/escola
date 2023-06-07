0bdc767cd9383445d2b53dda782c80a3078e4cbb
Bruno Leandro <115470852+BrnLLP@users.noreply.github.com>
2023-06-07 08:24:30 -0300

    Integração login.jsx com backend

--- sistemaescolar/frontend/src/components/Users/Cadastro.jsx
+++ sistemaescolar/frontend/src/components/Users/Cadastro.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import {
   MDBContainer,
@@ -14,20 +15,49 @@ import {
 from 'mdb-react-ui-kit'
 function App() {
   return (
+    <div id = "output">
+    <form id='form'>
     <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-      <MDBInput wrapperClass='mb-4' label='Usuário' id='form1' type='text'/>
-      <MDBInput wrapperClass='mb-4' label='Senha' id='form2' type='password'/>
-      <MDBInput wrapperClass='mb-4' label='Matricula' id='form2' type='text'/>
-      <MDBInput wrapperClass='mb-4' label='CPF' id='form2' type='text'/>
-      <MDBInput wrapperClass='mb-4' label='Cargo' id='form2' type='text'/>
+      <MDBInput wrapperClass='mb-4' label='Usuário' id='usuario' type='text'/>
+      <MDBInput wrapperClass='mb-4' label='Senha' id='senha' type='password'/>
+      <MDBInput wrapperClass='mb-4' label='Matricula' id='matricula' type='number'/>
+      <MDBInput wrapperClass='mb-4' label='CPF' id='cpf' type='text'/>
 
 
-      <MDBBtn className="mb-4">Cadastrar</MDBBtn>
+      <MDBBtn className="mb-4" id="btn">Cadastrar</MDBBtn>
 
   
 
     </MDBContainer>
+    </form>
+    </div>
+    <script> 
+    const form = document.getElementById('form');
+    const usuarioInput = document.getElementById('usuario');
+    const senhaInput = document.getElementById('senha');
+    const matriculaInput = document.getElementById('matricula');
+    const cpfInput = document.getElementById('cpf');
+  
+    btn.addEventListener("click", () => {
+     const usuario = usuarioInput.value;
+     const senha = senhaInput.value;
+     const matricula = matriculaInput.value;
+     const cpf = cpfInput.value;
+      
+     axios.post(''https://localhost:3000/aluno') , {
+          usuario: matricula,
+          senha : senha,
+          matricula : parseInt(matricula),
+          cpf : cpf
+                })
+     .then((response) => {
+           output.innterHTML = '<div> Cadastro realizado com sucesso  </div>'
+    }).catch(function (err){
+      document.getElementById('output')>innerHTML = '<div> Cadastro realizado com sucesso  </div>'
+    })
+    })
+    </script>
   );
 }
 
